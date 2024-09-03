abc6380b968edab6c1a715b980876d6bf88869f2
chenglong.zhang <chenglong.zhang@kunlun-inc.com>
2024-09-03 14:38:40 +0800

    init project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*
+ * @Author: chenglong.zhang chenglong.zhang@kunlun-inc.com
+ * @Date: 2024-09-03 14:11:59
+ * @LastEditors: chenglong.zhang chenglong.zhang@kunlun-inc.com
+ * @LastEditTime: 2024-09-03 14:19:44
+ * @FilePath: /rcli/src/main.rs
+ * @Description:
+ */
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
 }
